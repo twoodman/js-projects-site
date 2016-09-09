bcd830f96fb3536d69dd8170758eb5c479350c19
twoodman <tdwoodman@outlook.com>
2016-09-09 03:48:43 -0600

    finish fibonacci sequence project

---
+++ javascripts/js-fibseq.js
@@ -0,0 +1,124 @@
+// iife to protect global scope
+(function() {
+  // strict
+  'use strict';
+  // initialize output text
+  var outputText = '';
+  // get main output div
+  var outputMain = document.getElementById('main__output');
+
+  // add event listener to form submit event
+  document.getElementById('form__fibseq').addEventListener('submit', function(event) {
+    // prevent browser default when clicked
+    event.preventDefault();
+    // call checkInput function
+    checkInput();
+  });
+
+  // clear input
+  function clearInput() {
+    // just clears form submit value
+    document.getElementById('form__submit').value = '';
+  }
+
+  // check input
+  function checkInput() {
+    // get form submit element
+    var value = document.getElementById('form__submit').value;
+    // get form output element
+    var formOutput = document.getElementById('form__output');
+    // if form submit value is not a number
+    if (isNaN(value)) {
+      // output error msg
+      formOutput.innerHTML = '<span class="error">Numbers only, please.</span>';
+      // and clear input
+      clearInput();
+    } else {
+      // else clear error msg
+      formOutput.innerHTML = '';
+      // call submitForm with the value submitted in form
+      submitForm(value);
+      // and clear input
+      clearInput();
+    }
+  }
+
+  // form submit to generate fibonacci sequence
+  function submitForm(num) {
+    // while there are still children of outputMain element
+    while (outputMain.firstChild) {
+      // remove them
+      outputMain.removeChild(outputMain.firstChild);
+      /* note:
+      + this is the quickest way I've found
+      + to remove all children of a parent element
+      */
+    }
+    // call THE BIG BOY
+    getFibSequence(num);
+  }
+
+  /* GET YOU SOME FIBONACCI GOODNESS
+  + AKA: THE BIG BOY
+  */
+  function getFibSequence(num) {
+    // MAKE A NICE ARRAY
+    var arrFib = [];
+    /*
+    + set first two index to 0 and 1
+    + first two numbers of fib seq
+    */
+    arrFib[0] = 0;
+    arrFib[1] = 1;
+    // OUTPUT EM BOTH BEFORE LOOP
+    outputToDiv(arrFib[0], 'output');
+    outputToDiv(arrFib[1], 'output');
+    // loop through from 2 to given num
+    for (var i = 2; i <= num; i++) {
+      // push i to array THIS IS VERY IMPORTANT
+      arrFib.push(i);
+      /*
+      + DECLARE a new variable to store
+      + the SUM of the previous two
+      + index values.
+      + The first time this function runs,
+      + this will be the number 1.
+      + Because (2 - 1 = 1) + (2 - 2 = 0) = 1
+      */
+      var numAt = arrFib[i - 1] + arrFib[i - 2];
+
+
+      /*
+      + In the following, we are assigning to the CURRENT index:
+      + the value gripped* in numAt
+      + *(Yes, I read Eloquent JavaScript 2nd ed. recently)
+      + The first time this function runs,
+      + this number will be 1, which is, again,
+      + the number assigned to numAt in the latter assignment.
+      +
+      arrFib[i] = numAt;
+      // numAt equal to new value at current index
+      numAt = arrFib[i];
+      // output numAt (current arrFib index value)
+      outputToDiv(numAt, 'output');
+    }
+    // output the original sequence number to get
+    outputToDiv(' | FibSeq Position: ' + num, 'output__alt');
+  }
+
+  // output span function
+  function outputToDiv(text, type) {
+    // set output text to given arg
+    outputText = text;
+    // create span for i
+    var spanOutput = document.createElement('span');
+    // create text node for i
+    var outputContent = document.createTextNode(outputText);
+    // set span class to given arg
+    spanOutput.className = type;
+    // append outputcontent (outputtext node) to span
+    spanOutput.appendChild(outputContent);
+    // append span to main output div
+    outputMain.appendChild(spanOutput);
+  }
+}());
