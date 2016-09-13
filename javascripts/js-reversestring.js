de73a5b8d5bf1d80416fe4ff28362d76c4259f8c
twoodman <tdwoodman@outlook.com>
2016-09-13 00:25:15 -0600

    convert reverse string project to comply with standardJS

--- javascripts/js-reversestring.js
+++ javascripts/js-reversestring.js
@@ -1,48 +1,47 @@
 // iife to protect global scope
-(function() {
+(() => {
   // strict
-  'use strict';
+  'use strict'
   // get main output div
-  var outputMain = document.getElementById('main__output');
+  var outputMain = document.getElementById('main__output')
 
   // add event listener to form submit event
-  document.getElementById('form__reversestring').addEventListener('submit', function(event) {
-    event.preventDefault();
-    checkInput();
-  });
+  document.getElementById('form__reversestring').addEventListener('submit', (event) => {
+    event.preventDefault()
+    checkInput()
+  })
 
   // clear input
-  function clearInput() {
-    document.getElementById('form__submit').value = '';
+  function clearInput () {
+    document.getElementById('form__submit').value = ''
   }
 
   // check input
-  function checkInput() {
+  function checkInput () {
     // get form submit value
-    var value = document.getElementById('form__submit').value;
+    var value = document.getElementById('form__submit').value
     // get reverse string
-    getReverseString(value);
+    getReverseString(value)
     // clear input on submit
-    clearInput();
+    clearInput()
   }
 
   // reverse given string
-  function getReverseString(str) {
+  function getReverseString (str) {
     // remove previous
     while (outputMain.firstChild) {
-      outputMain.removeChild(outputMain.firstChild);
+      outputMain.removeChild(outputMain.firstChild)
     }
     // get main output
-    var formOutput = document.getElementById('main__output');
+    var formOutput = document.getElementById('main__output')
     // convert given string to array and reverse it
-    var arrayString = str.split('').reverse();
+    var arrayString = str.split('').reverse()
     // join reversed array to convert back to string
-    var reversedString = arrayString.join('');
+    var reversedString = arrayString.join('')
     /*
     + append reversed string to main output
     + this also escapes it by creating a text node
     */
-    formOutput.appendChild(document.createTextNode(reversedString));
+    formOutput.appendChild(document.createTextNode(reversedString))
   }
-
-}());
+})()
