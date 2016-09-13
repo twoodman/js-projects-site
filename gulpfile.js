75ef68042f1acedd3a772faa780f34dfc51cd5c6
twoodman <tdwoodman@outlook.com>
2016-09-13 00:25:28 -0600

    convert gulpfile to comply with standardJS

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,14 @@
 // require modules
-var gulp = require('gulp'),
-    // compiles Sass & compass
-    compass = require('gulp-compass'),
-    // minifies css
-    cleanCSS = require('gulp-clean-css'),
-    // renames files
-    rename = require('gulp-rename'),
-    // gulp plumber so errors dont break pipe
-    plumber = require('gulp-plumber'),
-    // webserver & live reload
-    webserver = require('gulp-webserver'),
-    // uglify for minifying js
-    uglify = require('gulp-uglify'),
-    // print to...print stuff...
-    print = require('gulp-print');
+const gulp = require('gulp')
+const compass = require('gulp-compass')
+const cleanCSS = require('gulp-clean-css')
+const rename = require('gulp-rename')
+const plumber = require('gulp-plumber')
+const webserver = require('gulp-webserver')
+const print = require('gulp-print')
 
 // compile Sass, minify, & output to stylesheets folder
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   gulp.src('scss/**/*.scss')
   .pipe(plumber())
   .pipe(compass({
@@ -29,41 +21,26 @@ gulp.task('sass', function() {
     suffix: '.min'
   }))
   .pipe(gulp.dest('dist/'))
-  .pipe(print(function(filepath) {
-    return 'built: ' + filepath;
-  }));
-});
-
-// minify, rename, output JS
-gulp.task('scripts', function() {
-  gulp.src('javascripts/*.js')
-  .pipe(plumber())
-  .pipe(uglify())
-  .pipe(rename({
-    suffix: '.min'
+  .pipe(print((filepath) => {
+    return 'built: ' + filepath
   }))
-  .pipe(gulp.dest('dist/'))
-  .pipe(print(function(filepath) {
-    return 'built: ' + filepath;
-  }));
-});
+})
 
 // open & live reload webserver
-gulp.task('reload', function() {
+gulp.task('reload', () => {
   gulp.src('./')
   .pipe(plumber())
   .pipe(webserver({
     open: true,
     auto: false,
     livereload: true
-  }));
-});
+  }))
+})
 
 //  watch task
-gulp.task('watch', function() {
-  gulp.watch('scss/**/*.scss', ['sass']);
-  gulp.watch('javascripts/*.js', ['scripts']);
-});
+gulp.task('watch', () => {
+  gulp.watch('scss/**/*.scss', ['sass'])
+})
 
 // default task
-gulp.task('do', ['scripts', 'sass', 'watch', 'reload']);
+gulp.task('do', ['sass', 'watch', 'reload'])
